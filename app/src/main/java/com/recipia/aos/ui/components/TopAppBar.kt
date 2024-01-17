dc0fb1c8a8c37253002cf65d0c2845da56879535
wlsdks <wlsdks12@naver.com>
2024-01-17 15:47:16 +0900

    [RCP-315] :recycle: topAppBar에 드롭다운 메뉴 추가

--- app/src/main/java/com/recipia/aos/ui/components/TopAppBar.kt
+++ app/src/main/java/com/recipia/aos/ui/components/TopAppBar.kt
@@ -1,41 +1,42 @@
 package com.recipia.aos.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.recipia.aos.ItemListWithInfiniteScroll
-import com.recipia.aos.ui.model.MyViewModel
-import com.recipia.aos.ui.theme.RecipiaaosTheme
+import androidx.navigation.NavController
+import com.recipia.aos.ui.components.infinitescroll.ItemListWithInfiniteScroll
+import com.recipia.aos.ui.components.menu.CustomDropdownMenu
+import com.recipia.aos.ui.model.RecipeAllListViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MediumTopAppBarExample(viewModel: MyViewModel = MyViewModel()) {
+fun TopAppBar(
+    navController: NavController,
+    viewModel: RecipeAllListViewModel
+) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    var menuExpanded by remember { mutableStateOf(false) } // 드롭다운 메뉴 상태
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-
         topBar = {
             CenterAlignedTopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -50,18 +51,37 @@ fun MediumTopAppBarExample(viewModel: MyViewModel = MyViewModel()) {
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = { navController.navigate("categories") }) {
                         Icon(
-                            imageVector = Icons.Filled.ArrowBack,
+                            imageVector = Icons.Filled.Menu,
                             contentDescription = "Localized description"
                         )
                     }
                 },
                 actions = {
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = { menuExpanded = true }) {
                         Icon(
-                            imageVector = Icons.Filled.Menu,
+                            imageVector = Icons.Filled.MoreVert,
                             contentDescription = "Localized description"
+                        )
+                    }
+                    CustomDropdownMenu(
+                        expanded = menuExpanded,
+                        onDismissRequest = { menuExpanded = false }
+                    ) {
+                        // 드롭다운 메뉴 아이템들
+                        DropdownMenuItem(
+                            text = { Text("Edit") },
+                            onClick = { /* Handle edit! */ }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Settings") },
+                            onClick = { /* Handle settings! */ }
+                        )
+                        DropdownMenuItem(
+                            text = {
+                                Text("Send Feedback") },
+                            onClick = { /* Handle send feedback! */ }
                         )
                     }
                 },
@@ -69,30 +89,6 @@ fun MediumTopAppBarExample(viewModel: MyViewModel = MyViewModel()) {
             )
         },
     ) { innerPadding ->
-        ItemListWithInfiniteScroll(viewModel, Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun ScrollContent(innerPadding: PaddingValues) {
-    val range = 1..100
-
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentPadding = innerPadding,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(range.count()) { index ->
-            Text(text = "- List item number ${index + 1}")
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewTopBar() {
-    RecipiaaosTheme {
-        MediumTopAppBarExample()
+        ItemListWithInfiniteScroll(viewModel)
     }
 }
