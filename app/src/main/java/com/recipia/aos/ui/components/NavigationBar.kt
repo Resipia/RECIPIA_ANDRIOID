dbadb76eb7bfd4d966a24b7765b55d89047a3bc8
wlsdks <wlsdks12@naver.com>
2024-01-17 15:48:56 +0900

    [RCP-315] :recycle: 네비게이션 바 리팩토링

--- app/src/main/java/com/recipia/aos/ui/components/NavigationBar.kt
+++ app/src/main/java/com/recipia/aos/ui/components/NavigationBar.kt
@@ -1,6 +1,6 @@
 package com.recipia.aos.ui.components
 
-import androidx.compose.foundation.layout.Row
+import android.widget.Toast
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material.icons.Icons
@@ -12,15 +12,14 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
-import com.recipia.aos.ui.theme.RecipiaaosTheme
+import androidx.navigation.NavController
+import java.util.Locale
 
 @Composable
 fun NavigationBar(
@@ -44,9 +43,10 @@ fun NavigationBar(
 }
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(navController: NavController) {
     val selectedItem = remember { mutableStateOf(0) }
-    val items = listOf("홈", "기획", "보던지", "말던지")
+    val context = LocalContext.current
+    val items = listOf("홈", "위글위글", "채팅", "마이페이지")
 
     NavigationBar {
         items.forEachIndexed { index, item ->
@@ -54,7 +54,20 @@ fun BottomNavigationBar() {
                 icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
                 label = { Text(item) },
                 selected = selectedItem.value == index,
-                onClick = { selectedItem.value = index }
+                onClick = {
+                    selectedItem.value = index
+                    if (item == "채팅") {
+                        Toast.makeText(context, "개발중인 기능입니다", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // 네비게이션 로직
+                        navController.navigate(when (item) {
+                            "홈" -> "home"
+                            "마이페이지" -> "mypage"
+                            // 다른 네비게이션 경로들
+                            else -> "home"
+                        })
+                    }
+                }
             )
         }
     }
