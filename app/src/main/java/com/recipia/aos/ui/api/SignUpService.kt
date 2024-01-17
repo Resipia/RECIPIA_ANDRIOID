745663e8038e1fffea83affccfe7cfb4de84eb61
wlsdks <wlsdks12@naver.com>
2024-01-17 15:45:59 +0900

    [RCP-315] :sparkles: 로그인 전용 인터페이스 객체 선언

---
+++ app/src/main/java/com/recipia/aos/ui/api/SignUpService.kt
@@ -0,0 +1,15 @@
+package com.recipia.aos.ui.api
+
+import com.recipia.aos.ui.dto.signup.ServerResponseDto
+import com.recipia.aos.ui.dto.signup.TokenMemberInfoDto
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface SignUpService {
+    // 로그인 요청을 보내는 POST 요청 정의
+    @POST("/auth/login")
+    fun login(
+        @Body tokenMemberInfoDto: TokenMemberInfoDto
+    ): Call<ServerResponseDto>
+}
