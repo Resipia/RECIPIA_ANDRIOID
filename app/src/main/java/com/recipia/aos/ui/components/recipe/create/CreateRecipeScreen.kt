78a24f3dfefc5f2e1e4b4d9198b96bd9e9f93593
wlsdks <wlsdks12@naver.com>
2024-01-19 16:54:11 +0900

    [RCP-327] :recycle: 레시피 생성 시 영양소 작성을 진행하는 컴포저블 추가

--- app/src/main/java/com/recipia/aos/ui/components/recipe/create/CreateRecipeScreen.kt
+++ app/src/main/java/com/recipia/aos/ui/components/recipe/create/CreateRecipeScreen.kt
@@ -1,16 +1,29 @@
 package com.recipia.aos.ui.components.recipe.create
 
+import TokenManager
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,15 +34,26 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.recipia.aos.ui.dto.recipe.NutritionalInfoDto
+import com.recipia.aos.ui.dto.recipe.RecipeCreateUpdateRequestDto
 import com.recipia.aos.ui.model.category.CategorySelectionViewModel
+import com.recipia.aos.ui.model.recipe.create.RecipeCreateModel
 
 
 /**
@@ -39,9 +63,9 @@ import com.recipia.aos.ui.model.category.CategorySelectionViewModel
 @Composable
 fun CreateRecipeScreen(
     navController: NavController,
-    viewModel: CategorySelectionViewModel
+    viewModel: CategorySelectionViewModel,
+    tokenManager: TokenManager
 ) {
-    val scaffoldState = rememberScaffoldState()
     val recipeName = remember { mutableStateOf("") }
     val recipeDesc = remember { mutableStateOf("") }
     val timeTaken = remember { mutableStateOf("") }
@@ -49,6 +73,25 @@ fun CreateRecipeScreen(
     val hashtag = remember { mutableStateOf("") }
     val nutritionalInfoList = remember { mutableStateListOf<NutritionalInfoDto>() }
     val showNutritionalInfo = remember { mutableStateOf(false) }
+    val context = LocalContext.current // 현재 컨텍스트를 가져옴
+
+    // 여기에서 레시피 생성 모델 인스턴스 생성
+    val model = RecipeCreateModel(tokenManager)
+
+    // 선택한 이미지 URI
+    var selectedImageUris by remember {
+        mutableStateOf<List<Uri?>>(emptyList())
+    }
+
+    val multiplePhotosPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickMultipleVisualMedia(
+            maxItems = 10
+        ),
+        onResult = {
+            selectedImageUris = it
+        }
+    )
+
 
     Scaffold(
         topBar = {
@@ -67,14 +110,101 @@ fun CreateRecipeScreen(
                 )
             )
         },
-    ) {
+        bottomBar = {
+            Button(
+                onClick = {
+                    // 데이터 전송 로직
+                    val lastNutritionalInfo = nutritionalInfoList.lastOrNull() ?: NutritionalInfoDto()
+                    val subCategoryDtoList = viewModel.createSubCategoryDtoList(viewModel.selectedCategories.value)
+
+                    val requestDto = RecipeCreateUpdateRequestDto(
+                        id = null,
+                        recipeName = recipeName.value,
+                        recipeDesc = recipeDesc.value,
+                        timeTaken = timeTaken.value.toIntOrNull() ?: 0,
+                        ingredient = ingredient.value,
+                        hashtag = hashtag.value,
+                        nutritionalInfo = lastNutritionalInfo,
+                        subCategoryDtoList = subCategoryDtoList,
+                        deleteFileOrder = listOf()
+                    )
+
+                    // 모델을 사용하여 서버로 데이터와 이미지 전송
+                    model.sendRecipeToServer(
+                        requestDto = requestDto,
+                        imageUris = selectedImageUris,
+                        context = context,
+                        onSuccess = {
+                            Toast.makeText(context, "레시피 생성 성공", Toast.LENGTH_SHORT).show()
+                            // 추가적인 성공 로직
+                        }
+                    ) { errorMessage ->
+                        Toast.makeText(context, "레시피 생성 실패: $errorMessage", Toast.LENGTH_LONG)
+                            .show()
+                        // 추가적인 실패 로직
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text("작성 완료")
+            }
+        }
+    ) { innerPadding ->
         LazyColumn(
             modifier = Modifier
+                .padding(innerPadding)
                 .fillMaxSize()
-                .padding(16.dp)
-                .padding(top = 56.dp), // TopAppBar의 높이(56.dp)만큼 추가 패딩을 제공하여 아래부터 시작
+                .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
+
+            // "이미지 선택" 버튼
+            item {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF0073E6),
+                        contentColor = Color.White
+                    ),
+                    onClick = {
+                        multiplePhotosPickerLauncher.launch(
+                            // 이미지만 선택 가능하도록
+                            PickVisualMediaRequest(
+                                ActivityResultContracts.PickVisualMedia.ImageOnly
+                            )
+                        )
+                    }) {
+
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = android.R.drawable.ic_input_add),
+                            contentDescription = "Add Image"
+                        )
+                        Text(
+                            text = "Pick Photos",
+                            style = TextStyle(
+                                fontSize = 18.sp
+                            )
+                        )
+                    }
+
+                }
+            }
+            item {
+                // 이미지 썸네일 목록
+                ImageThumbnails(selectedImageUris) { removedUri ->
+                    // 이미지 제거
+                    selectedImageUris = selectedImageUris.filter { it != removedUri }
+                }
+            }
             item {
                 TextField(
                     value = recipeName.value,
@@ -133,7 +263,7 @@ fun CreateRecipeScreen(
             // 영양소 입력 필드 영역 (버튼 누르면 필드 열림)
             if (showNutritionalInfo.value) {
                 item {
-                    NutritionalInfoInput(nutritionalInfoList.lastOrNull() ?: NutritionalInfoDto()) { updatedInfo ->
+                    NutritionalInfoInputScreen(nutritionalInfoList.lastOrNull() ?: NutritionalInfoDto()) { updatedInfo ->
                         nutritionalInfoList.add(updatedInfo)
                     }
                 }
@@ -155,8 +285,39 @@ fun CreateRecipeScreen(
                 Text("선택된 카테고리: ${viewModel.selectedCategories.value.joinToString()}")
             }
             // 이미지 업로드 UI 추가
+
         }
     }
 }
 
-// 이미지 업로드 및 기타 필요한 기능 구현
+@Composable
+fun ImageThumbnails(selectedImageUris: List<Uri?>, onRemoveImage: (Uri) -> Unit) {
+    LazyRow {
+        items(selectedImageUris) { uri ->
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(8.dp)
+            ) {
+                Image(
+                    painter = rememberImagePainter(uri),
+                    contentDescription = "Selected Image",
+                    modifier = Modifier.fillMaxSize()
+                )
+                // X 아이콘을 클릭하여 이미지 제거
+                IconButton(
+                    onClick = { onRemoveImage(uri!!) },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(4.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Remove Image",
+                        tint = Color.White
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/recipia/aos/ui/components/recipe/create/NutritionalInfoInputScreen.kt
+++ app/src/main/java/com/recipia/aos/ui/components/recipe/create/NutritionalInfoInputScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,9 +15,12 @@ import com.recipia.aos.ui.dto.recipe.NutritionalInfoDto
 
 /**
  * 영양소 입력 필드
+ * NutritionalInfoDto의 각 필드는 사용자가 입력을 할 때마다 즉시 업데이트되고 (onNutritionalInfoChanged(updatedInfo) 적용)
+ * 최종적으로 상위 컴포넌트나 ViewModel에 전달될 수 있게 되어,
+ * 사용자가 "저장" 버튼을 눌렀을 때 최신 상태의 데이터를 서버에 전송할 준비가 완료된다.
  */
 @Composable
-fun NutritionalInfoInput(
+fun NutritionalInfoInputScreen(
     nutritionalInfo: NutritionalInfoDto,
     onNutritionalInfoChanged: (NutritionalInfoDto) -> Unit
 ) {
@@ -31,66 +33,56 @@ fun NutritionalInfoInput(
         OutlinedTextField(
             value = nutritionalInfo.carbohydrates?.toString() ?: "",
             onValueChange = { text ->
-                onNutritionalInfoChanged(nutritionalInfo.copy(carbohydrates = text.toIntOrNull()))
+                val updatedInfo = nutritionalInfo.copy(carbohydrates = text.toIntOrNull())
+                onNutritionalInfoChanged(updatedInfo)
             },
             label = { Text("탄수화물 함량 (g)") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth() // 너비를 화면 전체로 설정
+            modifier = Modifier.fillMaxWidth()
         )
 
         OutlinedTextField(
             value = nutritionalInfo.protein?.toString() ?: "",
             onValueChange = { text ->
-                onNutritionalInfoChanged(nutritionalInfo.copy(protein = text.toIntOrNull()))
+                val updatedInfo = nutritionalInfo.copy(protein = text.toIntOrNull())
+                onNutritionalInfoChanged(updatedInfo)
             },
             label = { Text("단백질 함량 (g)") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth() // 너비를 화면 전체로 설정
+            modifier = Modifier.fillMaxWidth()
         )
 
         OutlinedTextField(
             value = nutritionalInfo.fat?.toString() ?: "",
             onValueChange = { text ->
-                onNutritionalInfoChanged(nutritionalInfo.copy(fat = text.toIntOrNull()))
+                val updatedInfo = nutritionalInfo.copy(fat = text.toIntOrNull())
+                onNutritionalInfoChanged(updatedInfo)
             },
             label = { Text("지방 함량 (g)") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth() // 너비를 화면 전체로 설정
+            modifier = Modifier.fillMaxWidth()
         )
 
         OutlinedTextField(
             value = nutritionalInfo.vitamins?.toString() ?: "",
             onValueChange = { text ->
-                onNutritionalInfoChanged(nutritionalInfo.copy(vitamins = text.toIntOrNull()))
+                val updatedInfo = nutritionalInfo.copy(vitamins = text.toIntOrNull())
+                onNutritionalInfoChanged(updatedInfo)
             },
             label = { Text("비타민 함량 (mg)") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth() // 너비를 화면 전체로 설정
+            modifier = Modifier.fillMaxWidth()
         )
 
         OutlinedTextField(
             value = nutritionalInfo.minerals?.toString() ?: "",
             onValueChange = { text ->
-                onNutritionalInfoChanged(nutritionalInfo.copy(minerals = text.toIntOrNull()))
+                val updatedInfo = nutritionalInfo.copy(minerals = text.toIntOrNull())
+                onNutritionalInfoChanged(updatedInfo)
             },
             label = { Text("미네랄 함량 (mg)") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth() // 너비를 화면 전체로 설정
+            modifier = Modifier.fillMaxWidth()
         )
-
-        // "영양소 입력하기" 버튼
-        Button(onClick = {
-            // 새로운 입력 필드를 추가하면서 현재 입력된 값을 보존합니다.
-            val newNutritionalInfo = NutritionalInfoDto(
-                carbohydrates = nutritionalInfo.carbohydrates,
-                protein = nutritionalInfo.protein,
-                fat = nutritionalInfo.fat,
-                vitamins = nutritionalInfo.vitamins,
-                minerals = nutritionalInfo.minerals
-            )
-            onNutritionalInfoChanged(newNutritionalInfo)
-        }) {
-            Text("영양소 입력하기")
-        }
     }
 }
