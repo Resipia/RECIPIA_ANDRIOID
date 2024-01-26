01b6155063a35592d0bc45940806689352e2ec1a
wlsdks <wlsdks12@naver.com>
2024-01-27 01:43:50 +0900

    [RCP-369] :recycle: 댓글이 sheet으로 보이도록 수정

--- app/src/main/java/com/recipia/aos/ui/components/recipe/detail/RecipeDetailScreen.kt
+++ app/src/main/java/com/recipia/aos/ui/components/recipe/detail/RecipeDetailScreen.kt
@@ -16,14 +16,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.AssistChip
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
@@ -31,10 +35,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -47,14 +53,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
-import coil.size.Scale
 import com.recipia.aos.ui.components.HorizontalDivider
 import com.recipia.aos.ui.components.menu.CustomDropdownMenu
 import com.recipia.aos.ui.components.recipe.detail.comment.CommentsSection
@@ -62,7 +65,7 @@ import com.recipia.aos.ui.model.comment.CommentViewModel
 import com.recipia.aos.ui.model.recipe.read.RecipeDetailViewModel
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun RecipeDetailScreen(
     recipeId: Long,
@@ -80,6 +83,30 @@ fun RecipeDetailScreen(
 
     // AlertDialog를 표시할지 여부를 관리하는 상태
     var showDialog by remember { mutableStateOf(false) }
+
+    // ModalBottomSheet 상태 관리
+    val sheetState = rememberModalBottomSheetState()
+    val coroutineScope = rememberCoroutineScope()
+    var showSheet by remember { mutableStateOf(false) }
+
+    // showSheet 상태를 변경하는 함수
+    val setShowSheet = { value: Boolean -> showSheet = value }
+
+    // 이것은 BottomSheet를 호출하는 부분이야.
+    if (showSheet) {
+        BottomSheet(
+            onDismiss = {  // 여기서 상태를 변경하는 람다 함수를 전달함
+                coroutineScope.launch {
+                    showSheet = false
+                }
+            },
+            commentViewModel = commentViewModel,
+            recipeId = recipeId
+        )
+    }
+
+    // 댓글 상태 관찰
+    val comments by commentViewModel.comments.collectAsState()
 
     if (showDialog) {
         AlertDialog(
@@ -115,6 +142,34 @@ fun RecipeDetailScreen(
         )
     }
 
+//    ModalBottomSheet(
+//        sheetState = sheetState,
+//        onDismissRequest = {
+//            coroutineScope.launch {
+//                sheetState.hide()
+//            }
+//        }
+//    ) {
+//        if (comments == null || comments!!.content.isEmpty()) {
+//            // 댓글 데이터가 없는 경우
+//            Text(
+//                text = "아직 작성된 댓글이 없습니다.",
+//                style = MaterialTheme.typography.bodyMedium,
+//                modifier = Modifier.padding(16.dp)
+//            )
+//        } else {
+//            // 댓글 데이터가 있는 경우
+//            CommentsSection(
+//                comments = comments!!.content,
+//                loadMoreComments = {
+//                    coroutineScope.launch {
+//                        commentViewModel.loadMoreComments(recipeId)
+//                    }
+//                }
+//            )
+//        }
+//    }
+
     Scaffold(
         containerColor = Color.White, // Scaffold의 배경색을 하얀색으로 설정
         topBar = {
@@ -128,7 +183,10 @@ fun RecipeDetailScreen(
                         navController.popBackStack()
                         navController.navigate("home")
                     }) {
-                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "뒤로 가기")
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "뒤로 가기"
+                        )
                     }
                 },
                 actions = {
@@ -181,7 +239,9 @@ fun RecipeDetailScreen(
             recipeDetailViewModel = recipeDetailViewModel,
             commentViewModel = commentViewModel,
             navController = navController,
-            paddingValues = innerPadding
+            paddingValues = innerPadding,
+            showSheet = showSheet,
+            setShowSheet = setShowSheet  // 상태 변경 함수 전달
         )
     }
 }
@@ -194,7 +254,9 @@ fun RecipeDetailContent(
     recipeDetailViewModel: RecipeDetailViewModel,
     commentViewModel: CommentViewModel,
     navController: NavController,
-    paddingValues: PaddingValues
+    paddingValues: PaddingValues,
+    showSheet: Boolean,
+    setShowSheet: (Boolean) -> Unit  // 상태 변경 함수 받기
 ) {
 
     // 레시피 상세 정보 로드
@@ -202,11 +264,6 @@ fun RecipeDetailContent(
         recipeDetailViewModel.loadRecipeDetail(recipeId)
         commentViewModel.loadInitialComments(recipeId) // 수정된 함수 호출
     }
-
-    val coroutineScope = rememberCoroutineScope() // 코루틴 스코프 생성
-
-    // 댓글 상태 관찰
-    val comments by commentViewModel.comments.collectAsState()
 
     // LiveData를 Compose에서 관찰하기 위해 observeAsState() 사용
     val recipeDetailState = recipeDetailViewModel.recipeDetail.observeAsState()
@@ -319,7 +376,8 @@ fun RecipeDetailContent(
                 Text(
                     text = "제목: ${recipeDetail.recipeName}",
                     style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier.padding(top = 8.dp)
+                    modifier = Modifier
+                        .padding(top = 8.dp)
                         .padding(horizontal = 16.dp)
                 )
 
@@ -379,26 +437,66 @@ fun RecipeDetailContent(
                     color = Color(222, 226, 230) // 구분선의 색상 설정
                 )
 
-                // 댓글 섹션
-                if (comments == null || comments!!.content.isEmpty()) {
-                    // 댓글 데이터가 없거나 댓글 목록이 비어있는 경우
-                    Text(
-                        text = "아직 작성된 댓글이 없습니다.",
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(16.dp)
-                    )
-                } else {
-                    // 댓글 데이터가 있는 경우
-                    CommentsSection(
-                        comments = comments!!.content,
-                        loadMoreComments = {
-                            coroutineScope.launch {
-                                commentViewModel.loadMoreComments(recipeId)
-                            }
-                        }
-                    )
+                // '댓글' 버튼을 눌렀을 때 showSheet 상태를 true로 설정
+                Button(
+                    onClick = { setShowSheet(true) },
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text("댓글")
                 }
+
             }
         }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet(
+    onDismiss: () -> Unit,  // 이 부분을 수정함
+    commentViewModel: CommentViewModel,
+    recipeId: Long
+) {
+    val modalBottomSheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = {
+            onDismiss()  // 여기서 onDismiss 함수를 호출함
+        },
+        sheetState = modalBottomSheetState,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+    ) {
+        CountryList(commentViewModel, recipeId)
+    }
+}
+
+
+@Composable
+fun CountryList(
+    commentViewModel: CommentViewModel,
+    recipeId: Long
+) {
+
+    val coroutineScope = rememberCoroutineScope()
+    val comments by commentViewModel.comments.collectAsState()
+
+    if (comments == null || comments!!.content.isEmpty()) {
+        // 댓글 데이터가 없는 경우
+        Text(
+            text = "아직 작성된 댓글이 없습니다.",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(16.dp)
+        )
+    } else {
+        // 댓글 데이터가 있는 경우
+        CommentsSection(
+            comments = comments!!.content,
+            loadMoreComments = {
+                coroutineScope.launch {
+                    commentViewModel.loadMoreComments(recipeId)
+                }
+            }
+        )
     }
 }
