ec20c8a4c61ae692e118698c4a9cf12a62673769
wlsdks <wlsdks12@naver.com>
2024-01-29 12:52:35 +0900

    [RCP-374] :recycle: 이미지 드래그 앤 드랍 및 디자인 수정

--- app/src/main/java/com/recipia/aos/ui/components/recipe/create/ImageThumbnails.kt
+++ app/src/main/java/com/recipia/aos/ui/components/recipe/create/ImageThumbnails.kt
@@ -6,26 +6,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import org.burnoutcrew.reorderable.ReorderableItem
-import org.burnoutcrew.reorderable.detectReorderAfterLongPress
+import org.burnoutcrew.reorderable.detectReorder
 import org.burnoutcrew.reorderable.rememberReorderableLazyListState
 import org.burnoutcrew.reorderable.reorderable
 
@@ -46,25 +45,32 @@ fun ImageThumbnails(
     LazyRow(
         state = state.listState,
         horizontalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier.reorderable(state).detectReorderAfterLongPress(state)
+        modifier = Modifier.reorderable(state)
     ) {
         items(selectedImageUris, { it.hashCode() }) { uri ->
             ReorderableItem(state, key = uri) { isDragging ->
                 Box(
                     modifier = Modifier
-                        .size(100.dp)
-                        .padding(8.dp)
+                        .size(72.dp) // 정사각형 크기 설정
+                        .padding(end = 4.dp)
                         .then(if (isDragging) Modifier.shadow(4.dp) else Modifier)
+                        .detectReorder(state)
                 ) {
                     uri?.let {
                         Image(
                             painter = rememberAsyncImagePainter(uri),
                             contentDescription = "Selected Image",
-                            modifier = Modifier.fillMaxSize().aspectRatio(1f)
+                            modifier = Modifier
+                                .fillMaxSize() // 이미지를 Box 안에서 꽉 채우게 설정
+                                .aspectRatio(1f), // 이미지를 정사각형 형태로 유지
+                            contentScale = ContentScale.Crop // 이미지가 비율을 유지하면서 영역을 꽉 채우도록 설정
                         )
                         IconButton(
                             onClick = { onRemoveImage(uri) },
-                            modifier = Modifier.align(Alignment.TopEnd).padding(4.dp)
+                            modifier = Modifier
+                                .align(Alignment.TopEnd)
+                                .offset(x = (11).dp, y = (-8).dp) // 아이콘 버튼을 우측 상단으로 조정
+                                .padding(0.dp) // 필요한 경우 패딩 조정
                         ) {
                             Icon(
                                 imageVector = Icons.Default.Close,
