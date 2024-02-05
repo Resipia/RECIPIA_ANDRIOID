02dfcecdd376c7e8c74152d1dd3094e8ac7d72a9
wlsdks <wlsdks12@naver.com>
2024-02-05 14:05:21 +0900

    [RCP-390] :recycle: 북마크 요청 jwt재발급 로직 추가

--- app/src/main/java/com/recipia/aos/ui/model/recipe/bookmark/BookMarkViewModel.kt
+++ app/src/main/java/com/recipia/aos/ui/model/recipe/bookmark/BookMarkViewModel.kt
@@ -1,13 +1,17 @@
 package com.recipia.aos.ui.model.recipe.bookmark
 
 import TokenManager
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.recipia.aos.BuildConfig
 import com.recipia.aos.ui.api.recipe.BookmarkService
 import com.recipia.aos.ui.dto.RecipeListResponseDto
 import com.recipia.aos.ui.dto.ResponseDto
 import com.recipia.aos.ui.dto.recipe.BookmarkRequestDto
+import com.recipia.aos.ui.model.jwt.TokenRepublishManager
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -28,6 +32,10 @@ class BookMarkViewModel(
     // 북마크 상태 업데이트를 위한 LiveData
     private val _bookmarkUpdateState = MutableLiveData<BookmarkUpdateState?>()
     val bookmarkUpdateState: MutableLiveData<BookmarkUpdateState?> get() = _bookmarkUpdateState
+
+    // 로그인 화면으로 이동해야 함을 알린다.
+    val _navigateToLogin = MutableLiveData<Boolean>()
+    val navigateToLogin: LiveData<Boolean> = _navigateToLogin
 
     // 북마크 요청 refrofit 설정 (로깅 인터셉터 추가)
     private val bookmarkService: BookmarkService by lazy {
@@ -55,17 +63,29 @@ class BookMarkViewModel(
     fun addBookmark(recipeId: Long) {
         val request = BookmarkRequestDto(recipeId)
         bookmarkService.addBookmark(request).enqueue(object : Callback<ResponseDto<Long>> {
-            // 응답 성공
+
             override fun onResponse(
                 call: Call<ResponseDto<Long>>,
                 response: Response<ResponseDto<Long>>
             ) {
+                // 응답 성공시 동작
                 if (response.isSuccessful) {
                     val newBookmarkId = response.body()?.result
                     newBookmarkId?.let { bookmarkId ->
-                        _bookmarkUpdateState.postValue(BookmarkUpdateState.Added(recipeId, bookmarkId))
+                        _bookmarkUpdateState.postValue(
+                            BookmarkUpdateState.Added(
+                                recipeId,
+                                bookmarkId
+                            )
+                        )
                         snackBarMessage.postValue("북마크에 추가되었습니다.")
                     }
+                // 만약 jwt의 access토큰이 만료되었다면
+                } else if (response.code() == 401) {
+                    handleUnauthorizedError {
+                        addBookmark(recipeId) // 토큰 재발급 후 북마크 추가 재시도
+                    }
+                // 다른 오류가 발생했다면
                 } else {
                     snackBarMessage.postValue("북마크 추가 실패: ${response.message()}")
                 }
@@ -87,9 +107,16 @@ class BookMarkViewModel(
                 call: Call<ResponseDto<Void>>,
                 response: Response<ResponseDto<Void>>
             ) {
+                // 응답 성공시 동작
                 if (response.isSuccessful) {
                     _bookmarkUpdateState.postValue(BookmarkUpdateState.Removed(bookmarkId))
                     snackBarMessage.postValue("북마크가 제거되었습니다.")
+                // 만약 jwt의 access토큰이 만료되었다면
+                } else if (response.code() == 401) {
+                    handleUnauthorizedError {
+                        removeBookmark(bookmarkId) // 토큰 재발급 후 북마크 제거 재시도
+                    }
+                // 다른 오류가 발생했다면
                 } else {
                     snackBarMessage.postValue("북마크 제거 실패: ${response.message()}")
                 }
@@ -105,18 +132,31 @@ class BookMarkViewModel(
         })
     }
 
-    // 북마크 상태 토글 함수
-    fun toggleBookmark(item: RecipeListResponseDto) {
-        item.bookmarkId?.let {
-            // bookmarkId가 있으면 북마크 제거
-            removeBookmark(it)
-        } ?: run {
-            // bookmarkId가 없으면 북마크 추가
-            addBookmark(item.id ?: return)
+    // 북마크 상태 업데이트
+    fun resetBookmarkUpdateState() {
+        _bookmarkUpdateState.value = null
+    }
+
+    /**
+     * 401 Unauthorized 에러 처리 및 토큰 재발급 로직
+     */
+    private fun handleUnauthorizedError(
+        retryAction: suspend () -> Unit
+    ) {
+        viewModelScope.launch {
+            val tokenRepublishManager = TokenRepublishManager(tokenManager)
+            val result = tokenRepublishManager.renewTokenIfNeeded()
+            if (result) {
+                retryAction() // 토큰 재발급 성공 시 전달받은 작업 재시도
+            } else {
+                // 토큰 재발급에 실패했다면, 로그인 화면으로 이동합니다.
+                _navigateToLogin.value = true
+            }
         }
     }
 
-    fun resetBookmarkUpdateState() {
-        _bookmarkUpdateState.value = null
+    // 홈 화면 이동 초기화
+    fun resetNavigateToLogin() {
+        _navigateToLogin.value = false
     }
 }
